144bb16e0524c4bb0f084759dbdb9178c55421a2
isbrqu <isbrqu@gmail.com>
2020-05-24 04:51:29 -0300

    limpia el contructor de Board

--- board.py
+++ board.py
@@ -6,15 +6,19 @@ YT = r'((?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|wa
 class Board:
 	"""docstring for Board"""
 	def __init__(self, element, subject_id):
-		youtube = re.findall(YT, element.get_attribute('innerHTML'))
-		self.urls = [{'name': 'Youtube video', 'url': url, 'subject_id': subject_id} for url in youtube]
-		board = element.find_elements_by_tag_name('a')
-		self.urls.extend([{'name': a.get_attribute('text'), 'url': a.get_attribute('href'), 'subject_id': subject_id}
-			for a in board if 'youtu' not in a.get_attribute('href')
-		])
-		self.urls = Collection(self.urls).unique('url').all()
+		self.urls = extract_urls(element, subject_id)
 		self.newurls = []
 
 	def diffurl(self, urls):
 		self.newurls = [url for url in self.urls if url['url'] not in urls]
 		return self.newurls
+
+    def extract_urls(self, element, subject_id):
+        youtube = re.findall(YT, element.get_attribute('innerHTML'))
+        urls = [{'name': 'Youtube video', 'url': url, 'subject_id': subject_id} for url in youtube]
+        board = element.find_elements_by_tag_name('a')
+        urls.extend([{'name': a.get_attribute('text'), 'url': a.get_attribute('href'), 'subject_id': subject_id}
+            for a in board if 'youtu' not in a.get_attribute('href')
+        ])
+        urls = Collection(urls).unique('url').all()
+        return urls
