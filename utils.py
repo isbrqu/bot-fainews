195b27414aa88dce4c89386989c1d62e7736434d
isbrqu <isbrqu@gmail.com>
2020-07-28 17:17:24 -0300

    coloca la url base en una constante

--- utils.py
+++ utils.py
@@ -1,8 +1,8 @@
-
-URL_LOGIN = 'https://pedco.uncoma.edu.ar/login/index.php'
-URL_MY = 'https://pedco.uncoma.edu.ar/my/'
-URL_COURSE = 'https://pedco.uncoma.edu.ar/course/view.php?id=%d'
-URL_FORUM = 'https://pedco.uncoma.edu.ar/mod/forum/view.php?id=%d'
+URL_BASE = 'https://pedco.uncoma.edu.ar/'
+URL_LOGIN = URL_BASE + 'login/index.php'
+URL_MY = URL_BASE + 'my/'
+URL_COURSE = URL_BASE + 'course/view.php?id=%d'
+URL_FORUM = URL_BASE + 'mod/forum/view.php?id=%d'
 
 # espaca caracteres especiales de markdown de telegram
 def escape(text):
