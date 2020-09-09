11c07c3697fbd56b3a3bc872914608c29051ff5a
Isaac Bruno Quiroga <isbrqu@gmail.com>
2020-09-09 18:26:50 -0300

    elimina el archivo urlp.py

--- urlp.py
+++
@@ -1,6 +0,0 @@
-URL_BASE = 'https://pedco.uncoma.edu.ar/'
-URL_LOGIN = URL_BASE + 'login/index.php'
-URL_HOME = URL_BASE + 'my/'
-URL_COURSE = URL_BASE + 'course/view.php?id=%d'
-URL_FORUM = URL_BASE + 'mod/forum/view.php?id=%d'
-
