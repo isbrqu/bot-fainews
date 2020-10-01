35dc4fa723fb4f03738a153c675d63a2bd0b9c75
Isaac Bruno Quiroga <isbrqu@gmail.com>
2020-10-01 00:28:54 -0300

    modulariza mejor el método screenshot

--- pedco/selenium_pedco.py
+++ pedco/selenium_pedco.py
@@ -3,23 +3,29 @@ from io import BytesIO
 from datetime import datetime
 from .selenium_moodle import SeleniumMoodle
 
+FOLDER = 'img'
+
 class SeleniumPedco(SeleniumMoodle):
 
     def __init__(self):
         super().__init__()
 
-    def screenshot_article(self, forum):
-        self.open_with_session(forum.url)
-        dt = datetime.now().strftime('%m-%d-%H-%M-%S')
-        path = f'img/{name}-{dt}.png'
-        Image.open(
-            BytesIO(self.get_screenshot_as_png())
-        ).crop(
-            self._calculate_dimensions(
-                self.find_element_by_tag_name('article')
-            )
-        ).save(path)
+    def screenshot(self, url, name):
+        self.open_with_session(url)
+        dimensions = self._calculate_dimensions_discussion()
+        path = self._generate_path(name)
+        Image.open(BytesIO(self.get_screenshot_as_png()))\
+            .crop(dimensions)\
+            .save(path)
         return path
+
+    def _generate_path(self, name):
+        date = datetime.now().strftime('%m-%d-%H-%M-%S')
+        return f'{FOLDER}/{name}-{date}.png' 
+
+    def _calculate_dimensions_discussion(self):
+        tag = self.find_element_by_tag_name('article')
+        return self._calculate_dimensions(tag)
 
     def _calculate_dimensions(self, tag):
         location, size = tag.location, tag.size
